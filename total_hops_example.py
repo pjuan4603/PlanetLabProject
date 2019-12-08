d9d6a7bd1b62b049109468b8972deda55ae95227
Po-Cheng Juan <43415804+pjuan4603@users.noreply.github.com>
2019-12-08 02:05:02 -0500

    Update and rename test.py to total_hops_example.py

--- total_hops_example.py
+++ total_hops_example.py
@@ -3,4 +3,4 @@ import json
 with open('waseda.json') as json_file:
     data = json.load(json_file)
     
-    print len(data['12_03_2019']['trace']['82']['hops'])
+    print data['12_03_2019']['trace']['82']['total_hops']
